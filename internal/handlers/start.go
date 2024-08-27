3375acd786231294e60eaee8b4c2b5a7a42d658d
1g0rbm <m1g0rb89@gmail.com>
2024-08-27 21:55:39 +0300

    Moved handling enbled user code to separate function.

--- internal/handlers/start.go
+++ internal/handlers/start.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
-	"wireguard-api/internal/repository/user"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 
+	"wireguard-api/internal/repository/user"
 	"wireguard-api/internal/services"
 	"wireguard-api/internal/utils"
 )
@@ -84,38 +84,8 @@ func (h *StartHandler) Handle(ctx context.Context, b *bot.Bot, update *models.Up
 		return
 	}
 
-	fmt.Println(userModel)
-	fmt.Println(userModel.State)
-	fmt.Println(userModel.StateIs(user.EnabledState))
-
 	if userModel != nil && userModel.StateIs(user.EnabledState) {
-		var keyboard *models.ReplyKeyboardMarkup
-		if userModel.RoleIs(user.AdminRole) {
-			keyboard = adminConfigKeyboard
-		} else {
-			keyboard = configKeyboard
-		}
-
-		utils.SendMessage(
-			func() ([]byte, error) {
-				return utils.Render(
-					"static/messages/user_already_enabled.tmp",
-					map[string]string{"Username": update.Message.Chat.Username},
-				)
-			},
-			func(msg []byte) error {
-				_, err := b.SendMessage(ctx, &bot.SendMessageParams{
-					ChatID:      update.Message.Chat.ID,
-					Text:        string(msg),
-					ParseMode:   models.ParseModeMarkdown,
-					ReplyMarkup: keyboard,
-				})
-				if err != nil {
-					return fmt.Errorf("handler_start.handle %w", err)
-				}
-				return nil
-			},
-		)
+		handleEnabledUser(ctx, b, update, userModel)
 		return
 	}
 
@@ -155,6 +125,51 @@ func (h *StartHandler) Handle(ctx context.Context, b *bot.Bot, update *models.Up
 				ChatID:    update.Message.Chat.ID,
 				Text:      string(msg),
 				ParseMode: models.ParseModeMarkdown,
+			})
+			if err != nil {
+				return fmt.Errorf("handler_start.handle %w", err)
+			}
+			return nil
+		},
+	)
+}
+
+func handleEnabledUser(ctx context.Context, b *bot.Bot, update *models.Update, userModel *user.Model) {
+	var keyboard *models.ReplyKeyboardMarkup
+	if userModel.RoleIs(user.AdminRole) {
+		keyboard = &models.ReplyKeyboardMarkup{
+			Keyboard: [][]models.KeyboardButton{
+				{{Text: configCommand}},
+				{{Text: qrCodeCommand}},
+				{{Text: "GetDisabledUserList"}},
+			},
+			ResizeKeyboard:  true,
+			OneTimeKeyboard: false,
+		}
+	} else {
+		keyboard = &models.ReplyKeyboardMarkup{
+			Keyboard: [][]models.KeyboardButton{
+				{{Text: configCommand}},
+				{{Text: qrCodeCommand}},
+			},
+			ResizeKeyboard:  true,
+			OneTimeKeyboard: false,
+		}
+	}
+
+	utils.SendMessage(
+		func() ([]byte, error) {
+			return utils.Render(
+				"static/messages/user_already_enabled.tmp",
+				map[string]string{"Username": update.Message.Chat.Username},
+			)
+		},
+		func(msg []byte) error {
+			_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID:      update.Message.Chat.ID,
+				Text:        string(msg),
+				ParseMode:   models.ParseModeMarkdown,
+				ReplyMarkup: keyboard,
 			})
 			if err != nil {
 				return fmt.Errorf("handler_start.handle %w", err)
